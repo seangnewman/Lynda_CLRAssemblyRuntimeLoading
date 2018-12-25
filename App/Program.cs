5f488a71d7a36258a45b32bdfde7a9ead36b0f26
newmanuevers <seangnewman@msn.com>
2018-12-25 05:59:01 -0700

    Assembly Resolution and Policies

---
+++ App/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var person = new Person() {
+                FirstName = "Sean",
+                LastName = "Newman",
+                Age = 56
+            };
+
+            Console.WriteLine($"Person = {person.FirstName}");
+        }
+    }
+}
---
+++ Lib/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib
+{
+    public class Person
+    {
+
+        static Person()
+        {
+            System.Reflection.Assembly thisAssembly = typeof(Person).Assembly;
+            Console.WriteLine(thisAssembly.CodeBase);
+            Console.WriteLine(thisAssembly.Location);
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"[Person FN:{FirstName} LN: {LastName} A: {Age}");
+        }
+    }
+}
